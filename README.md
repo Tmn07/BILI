6fe84d0fa3cd436c68cbf41c5e4b9aff6e59a9d3
tmn07 <519043202@qq.com>
2016-08-11 23:02:01 +0800

    版本2

--- README.md
+++ README.md
@@ -1,19 +1,36 @@
-##2233##
+## 2233
 
-- 目前是爬取指定av号的弹幕存到本地。原理是贴吧上看到的2333
-- 弹幕信息有内容，时间，和时间2333
-- hackathon甩锅做这个真的好吗2333
-
-
-
+- 目前是爬取指定av号的弹幕存到本地。原理是贴吧上看到的2233
+- 弹幕信息有内容，时间，和时间2233
+- 当初hackPKU的第三天上午撸了2,3小时的个说2233
 - 使用方法 python bilibili.py av_number
   - 例如 python bilibili.py 10492 （这个资源被封了。好像有办法拿到。但不是我的关注点
   - python bilibili.py 1314520
+- 合集的弹幕似乎有问题下一个commit再处理。
+- 之后可能尝试做个api。。
+
+## 说明
+
+### 示例
+![python bilibili.py 5227685](./res/use.png)
+
+### 怎么发现的
+
+- 我是贴吧里发现这个的秘密2233
+- 刚刚才看到那个包2233![xml](./res/response.png)
+- 然后全局找一下这串数字2233![cid](./res/cid.png)
+
+### 压缩的理解
+
+-  request 发送的'Accept-Encoding': 'gzip' 告诉服务器我这里支持这几种压缩的流？然后服务器支持的话会选择一种发。
+-  听说 不加这一条会放回原始的数据，但是B站不行
+-  response里可能会有。Content-Encoding:gzip 表示发给你的是gzip压缩的。
+-  python中用 response.info().get('Content-Encoding')获取
+-  视频页面是'gzip'压缩传过来的
+-  弹幕xml是'deflate'压缩传过来的
+
+### 版本说明
+- 第一个版本，xml是用xml.dom.minidom这个库来解析的，看前三个commit
+- 这个版本，cid用这正则来获取，加上了header（然并卵），bs4来解析xml
 
 
-
-- 注意点：
-  - 代码写的很烂。。想读的。联系我2333
-  - 网页可能经过压缩。。爬下来需解压缩，gzip,zlib,det???（坑。。(貌似是我没写header的锅。。尽量模拟浏览器再爬吧
-  - xml。。姿势不对。。我先把它下载下来再读取
-  - 中文。。编码。。一直的硬伤
--- bilibili.py
+++ bilibili.py
@@ -11,9 +11,6 @@ import gzip
 import zlib
 import re
 
-from xml.dom.minidom import parse
-import xml.dom.minidom
-  
 import time
 
 import sys
@@ -23,69 +20,73 @@ sys.setdefaultencoding( "utf-8" )
 class BILI(object):
     def __init__(self):
         pass
+
     def gzip_url(self,url):
-        response = urllib2.urlopen(url)
+        header = {
+            'Accept-Language': 'zh-CN,zh;q=0.8',
+            'Accept-Encoding': 'gzip',
+            'Referer': 'http://www.bilibili.com/',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:45.0) Gecko/20100101 Firefox/45.0'
+        }
+        request = urllib2.Request(url,headers=header)
+        response = urllib2.urlopen(request)
+        # gzip解压
         if response.info().get('Content-Encoding') == 'gzip':
             compresseddata = response.read()
             buf = StringIO(compresseddata)
             f = gzip.GzipFile(fileobj=buf)
             data = f.read()
+        # deflate解压
         else:
             data = zlib.decompress(response.read(),-zlib.MAX_WBITS)
         return data
 
     def set_url(self,url):
         html = self.gzip_url(url)
-        soup = BeautifulSoup(html,"html.parser", from_encoding="utf-8")   
+        print '视频页面 gzip解压完成...'
+
+        soup = BeautifulSoup(html,"lxml")
         try:
             da1 = soup.find('div',id="bofqi")
-            jsstring = unicode(da1.script.string)
-        
-            ####之后用正则改。。慢？
-            cidstring = jsstring.split("\"")[3]
-            cid = int(cidstring[4])
-    
-            for i in range(5,len(cidstring)):
-                char = cidstring[i]
-                if char.isdigit():
-                    cid = cid*10+int(char)
-                else:
-                    break
-            self.cid = str(cid)
-            return cid
+            jsstring = da1.script.string
+
+            p = re.compile(r'cid=\d+&')
+            cid = p.findall(jsstring)[0][4:-1]
+            print 'cid获取完成...'
+
         except Exception , e:
             print 'something serious happened  ->',
             print e
             exit()
+
+        self.get_danmu(cid)
         
-    def get_danmu(self):
-        danmu_url = "http://comment.bilibili.com/"+self.cid+".xml"
+    def get_danmu(self,cid):
+        danmu_url = "http://comment.bilibili.com/"+cid+".xml"
+
         data = self.gzip_url(danmu_url)
-        fd = open("danmu.xml",'w')
-        fd.write(data)        
-        fd.close()
-        print "XML has been write down from internet"
-        root = xml.dom.minidom.parse("danmu.xml")
-        ds = root.getElementsByTagName('d')
+        print("弹幕页面 deflate解压完成...")
+        # fd = open("danmu.xml",'w')
+        # fd.write(data)
+        # fd.close()
+
+        soup = BeautifulSoup(data,'lxml')
+        danmus = soup.find_all('d')
         fw = open('danmu.txt','w')
-        print "XML reading.."
-        for i in range(len(ds)):
-            try:
-                fw.write(ds[i].firstChild.data)
-                fw.write('\t')
-                timelist = ds[i].getAttribute("p").split(',')
-                x = time.localtime(float(timelist[4]))
-                timestr = time.strftime('%Y-%m-%d %H:%M:%S',x)
-                fw.write(timelist[0])
-                fw.write('\t')
-                fw.write(timestr)
-                fw.write('\n')
-            except Exception , e:
-                fw.write('\n')
-                print 'something wrong happened but not serious ->',
-                print e
+        print("写入弹幕ing...")
+        for danmu in danmus:
+            content = danmu.string
+
+            attr = danmu['p'].split(',')
+            t1 = attr[0] # 视频中的时间 
+            t2 = attr[4] # 发布时间
+            timestr = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(float(t2)))
+
+            fw.write(content+'\t'+t1+'\t'+timestr+'\n')
+
         fw.close()
-        
+        print("写入完成...请查看danmu.txt")
+
         
 def main():
     print "script_name:", sys.argv[0]
@@ -93,9 +94,6 @@ def main():
     print "av_number:",sys.argv[1],"parepering"
     url = r"http://www.bilibili.com/video/av"+sys.argv[1]
     b1.set_url(url)
-    print "cid_completed"
-    b1.get_danmu()
-    print "Completed.And resource in danmu.xml,danmu.txt"
 
 if __name__ == '__main__':
     main()
